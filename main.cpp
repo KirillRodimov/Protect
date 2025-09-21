6de17f56c2bbb9b128b4fdf4a976a014f88c4c42
KirillRodimov <kirill.a.rodimov@gmail.com>
2025-09-21 13:09:46 +0300

    db connected

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include "mainwindow.h"
+#include <stdio.h>
+extern "C" {
+#include "sqlite3.h"
+}
 
 #include <QApplication>
 
@@ -7,5 +11,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    sqlite3 *db;    // указатель на базу данных
+    // открываем подключение к базе данных
+    int result  = sqlite3_open("test.db", &db);
+    printf("result = %d\n", result);
+    // закрываем подключение
+    sqlite3_close(db);
     return a.exec();
 }
